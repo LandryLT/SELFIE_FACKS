557bd3112e11ab1b0c8988580276da10a0155044
LandryLT <59315119+LandryLT@users.noreply.github.com>
2026-09-13 14:34:19 +0200

    global var

--- main.py
+++ main.py
@@ -48,6 +48,7 @@ def get_cam_settings(conf_file: Path = Path("./cam_settings.json")) -> dict:
 
 @app.route("/lamp", methods=['GET'])
 def switchLamp(mode: bool = led_is_on):
+    global led_is_on
     GPIO.output(pin, GPIO.LOW if mode else GPIO.HIGH)
     led_is_on = not mode
     return make_response(str(led_is_on), 200)
